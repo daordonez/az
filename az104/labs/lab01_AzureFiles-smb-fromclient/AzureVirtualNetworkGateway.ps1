7618cbabfb8650787dc972ed97db861d5de3df98
Diego Ordoñez <d.ordonez.morales@avanade.com>
2022-11-21 15:47:32 +0100

    first commit. Added az labs and practices

---
+++ az104/labs/lab01_AzureFiles-smb-fromclient/AzureVirtualNetworkGateway.ps1
@@ -0,0 +1,32 @@
+$vpnName = "vpn-azfiles" 
+$publicIpAddressName = "$vpnName-PublicIP"
+$region = "westeurope"
+$gatewaySubnet = "default"
+
+$publicIPAddress = New-AzPublicIpAddress `
+    -ResourceGroupName $resourceGroupName `
+    -Name $publicIpAddressName `
+    -Location $region `
+    -Sku Basic `
+    -AllocationMethod Dynamic
+
+$gatewayIpConfig = New-AzVirtualNetworkGatewayIpConfig `
+    -Name "vnetGatewayConfig" `
+    -SubnetId $gatewaySubnet.Id `
+    -PublicIpAddressId $publicIPAddress.Id
+
+$azRootCertificate = New-AzVpnClientRootCertificate `
+    -Name "P2SRootCert" `
+    -PublicCertData $rootCertificate
+
+$vpn = New-AzVirtualNetworkGateway `
+    -ResourceGroupName $resourceGroupName `
+    -Name $vpnName `
+    -Location $region `
+    -GatewaySku VpnGw1 `
+    -GatewayType Vpn `
+    -VpnType RouteBased `
+    -IpConfigurations $gatewayIpConfig `
+    -VpnClientAddressPool "172.16.201.0/24" `
+    -VpnClientProtocol IkeV2 `
+    -VpnClientRootCertificates $azRootCertificate
